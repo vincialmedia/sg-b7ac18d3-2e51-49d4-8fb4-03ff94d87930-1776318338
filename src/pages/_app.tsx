5950b0f63022fa5e442adfc5fe8992621d052890
softgenai <admin@softgen.ai>
2025-06-05 10:54:07 +0000

    Edit src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,26 @@
-
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-   
-<script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/146320474.js"></script>
+    <>
+      {/* HubSpot */}
+      <Script
+        id="hs-script-loader"
+        strategy="afterInteractive"
+        src="//js-eu1.hs-scripts.com/146320474.js"
+      />
 
-       <script 
-          src="https://cdn.softgen.ai/script.js" 
-          async 
-          data-softgen-monitoring="true"
-        />
+      {/* Softgen */}
+      <Script
+        src="https://cdn.softgen.ai/script.js"
+        strategy="afterInteractive"
+        data-softgen-monitoring="true"
+      />
+
+      <Component {...pageProps} />
+    </>
   )
 }
 
