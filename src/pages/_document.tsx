b258f57b6cc1b4a0f739761da0175acef11bf452
softgenai <admin@softgen.ai>
2025-06-05 10:49:24 +0000

    Edit src/pages/_document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-          <Script
+      <Script
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid="093175ce-ab1b-45f1-b766-f12aa6311a07"
+        strategy="beforeInteractive"
+      />
+      <Script
         id="hs-script-loader"
         src="//js-eu1.hs-scripts.com/146320474.js"
         strategy="afterInteractive"
