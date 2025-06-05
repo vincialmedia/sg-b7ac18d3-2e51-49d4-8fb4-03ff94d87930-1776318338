52ca59d945c1dbbb17b570288f7bf615ff3325b0
softgenai <admin@softgen.ai>
2025-06-05 10:49:08 +0000

    Edit src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,12 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        id="Cookiebot"
-        src="https://consent.cookiebot.com/uc.js"
-        data-cbid="093175ce-ab1b-45f1-b766-f12aa6311a07"
-        strategy="beforeInteractive"
-      />
-      <Script
-        id="hs-script-loader"
-        src="//js-eu1.hs-scripts.com/146320474.js"
-        strategy="afterInteractive"
-      />
+
+       <script 
+          src="https://cdn.softgen.ai/script.js" 
+          async 
+          data-softgen-monitoring="true"
+        />
       <Component {...pageProps} />
     </>
   )
