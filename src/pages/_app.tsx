3cb429cb962fc66876e61c74d0ba750fdf36b56b
softgenai <admin@softgen.ai>
2025-06-05 11:17:36 +0000

    Edit src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Cookiebot Script */}
+      <Script
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid="093175ce-ab1b-45f1-b766-f12aa6311a07"
+        type="text/javascript"
+        strategy="beforeInteractive"
+      />
+
+      {/* HubSpot Script */}
+      <Script
+        id="hs-script-loader"
+        src="//js-eu1.hs-scripts.com/146320474.js"
+        strategy="afterInteractive"
+        async
+        defer
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
