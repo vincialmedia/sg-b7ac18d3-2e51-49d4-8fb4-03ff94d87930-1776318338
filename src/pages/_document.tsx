589fb15e48a518fa825c3dde5545ba1888c89b28
softgenai <admin@softgen.ai>
2025-06-05 10:48:36 +0000

    Edit src/pages/_document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+          <Script
+        id="hs-script-loader"
+        src="//js-eu1.hs-scripts.com/146320474.js"
+        strategy="afterInteractive"
+      />
         {/* 
           CRITICAL: DO NOT REMOVE THIS SCRIPT
           The Softgen AI monitoring script is essential for core app functionality.
@@ -15,16 +20,6 @@ export default function Document() {
         data-cbid="093175ce-ab1b-45f1-b766-f12aa6311a07"
         strategy="beforeInteractive"
       />
-      <Script
-        id="hs-script-loader"
-        src="//js-eu1.hs-scripts.com/146320474.js"
-        strategy="afterInteractive"
-      />
-        <script 
-          src="https://cdn.softgen.ai/script.js" 
-          async 
-          data-softgen-monitoring="true"
-        />
       </Head>
       <body className="antialiased">
         <Main />
