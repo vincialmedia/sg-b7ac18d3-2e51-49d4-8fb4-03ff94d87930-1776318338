3cea93804f795394101a7180018d02358ffcd092
softgenai <admin@softgen.ai>
2025-06-05 10:34:43 +0000

    Edit src/pages/_document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,17 @@ export default function Document() {
           async 
           data-softgen-monitoring="true"
         />
+        <Script
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid="093175ce-ab1b-45f1-b766-f12aa6311a07"
+        strategy="beforeInteractive"
+      />
+      <Script
+        id="hs-script-loader"
+        src="//js-eu1.hs-scripts.com/146320474.js"
+        strategy="afterInteractive"
+      />
       </Head>
       <body className="antialiased">
         <Main />
